32d9389ec2cb3ac71d29b003134f5592fffb3de7
Annleen084Su <6780182127@student.chula.ac.th>
2025-12-04 09:16:05 +0700

    Set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Health-Me-Please/',
   plugins: [react()],
   // สำคัญมาก: ชี้ให้ถูก path ของ GitHub Pages
   base: '/Health-Me-Please/',
